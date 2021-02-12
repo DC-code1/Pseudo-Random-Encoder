61e8692adc97f9820993044672a37bf1bf1a18db
DC-code1 <77569305+DC-code1@users.noreply.github.com>
2021-02-12 15:23:05 +1100

    Update 2.1.py

--- 2.1.py
+++ 2.1.py
@@ -1,55 +1,35 @@
-import textwrap
-import random
-random.seed(202090)
-table1 = []
-for i in range(26):
-    table1.append(i)
-
-letters = 'abcdefghijklmnopqrstuvwxyz'
-letter_list = list(letters)
-dict1 = dict(zip(table1,letter_list))
-
-num = []
-for i in range(26):
-    num.append(i)
-#print(num)
-rannum = random.shuffle(table1)
-
-key_list = list(dict1.keys())
-val_list = list(dict1.values())
-
-l = []
-for i in range(len(letters)):
-    nummsg = table1[i]
-    x = val_list[nummsg].upper()
-    l.append(x)
-    #print(x,end=' ')
-#print(l)
-    
 mydict = dict(zip(letters,l))
 #print(mydict)
 #msg = 'Hello World! Welcome to ITC106, Session 1 of 2021. All the best :)'
 #msg = "ITC106: Programming Principles"
 msg = ''
-with open('Untitled_asn2.txt') as x:
-    for line in x:
-        line = line.rstrip()
-        textwrap.wrap(msg, width=50)
+with open('Untitled_asn2.txt','r') as x:
+    lines = x.readline()
+    for lines in x:
+        #lines = lines.rstrip() # Not needed as items are in different lines.
+        msg += lines
+        
     #print(lines)
-    msg += line
-textwrap.wrap(msg, width=50)
 print('Original content.')
-print(msg)
-print()
+wrappedlines = textwrap.wrap(msg,width = 100)
+for lines in wrappedlines:
+    print(lines)
     
+#print(msg)
+print()
+print('Encoded message.')
+
 encoded = ''
 for letter in msg.lower():
     if letter in mydict:
         encoded += mydict[letter]
     else:
         encoded += letter
-print('Encoded message.')
-print(encoded,end='')
+wrappedlines = textwrap.wrap(encoded,width = 100)
+for lines in wrappedlines:
+    print(lines)
+
 print()
 print(mydict)
+
 
